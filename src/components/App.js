887d0b2afb284df2836e67ea84ad55d99a6c703c
Courtney Noonan <courtneynoonan@me.com>
2017-09-13 10:32:36 -0400

    adding dev dependencies, react router and file structure skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import '../App.css'
 
 class App extends Component {
   render() {
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         Hello World!
       </div>
-    );
+    )
   }
 }
 
