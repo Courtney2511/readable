589ca25184a26b7de83b99c8f86c70f4f4e17114
Courtney Noonan <courtneynoonan@me.com>
2017-09-13 16:04:08 -0400

    adding redux, creating store and reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import * as actions from '../actions'
+
+const initialState = {}
+
+function posts (state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default posts
