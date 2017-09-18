5f8ba363b527a81d8a6c8eca0365f8ca6d7df327
Courtney Noonan <courtneynoonan@me.com>
2017-09-18 11:49:05 -0400

    refactoring post list to use a Post component

---
+++ src/components/Post.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import FormattedDate from '../helpers/FormattedDate'
+import VoteScore from './VoteScore'
+
+
+class Post extends Component {
+
+
+  render() {
+    return (
+      <div key = { this.props.post.id }className='post-container'>
+        <div className='post-left'>
+          <div className='rank'>
+            <span>1</span>
+          </div>
+          <VoteScore score={ this.props.post.voteScore }/>
+        </div>
+        <div className='post-middle'>
+          <p>{this.props.post.title}</p>
+          <p>{this.props.post.body}</p>
+          <span className='subscript'>
+            <p>submitted @ <FormattedDate date={this.props.post.timestamp} /> by {this.props.post.author}</p>
+            <p># comments</p>
+          </span>
+        </div>
+        <div className='post-right'>
+          <p>edit</p>
+          <p>delete</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Post
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,38 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getPosts } from '../actions'
-import FormattedDate from '../helpers/FormattedDate'
-import VoteScore from './VoteScore'
+import Post from './Post'
 
 class PostList extends Component {
 
   componentDidMount() {
     this.props.getPosts()
   }
+
   render() {
     return (
       <div className='posts-container'>
         {this.props.posts.map(post =>
-          <div key = { post.id }className='post-container'>
-            <div className='post-left'>
-              <div className='rank'>
-                <span>1</span>
-              </div>
-              <VoteScore score={ post.voteScore }/>
-            </div>
-            <div className='post-middle'>
-              <p>{post.title}</p>
-              <p>{post.body}</p>
-              <span className='subscript'>
-                <p>submitted @ <FormattedDate date={post.timestamp} /> by {post.author}</p>
-                <p># comments</p>
-              </span>
-            </div>
-            <div className='post-right'>
-              <p>edit</p>
-              <p>delete</p>
-            </div>
-          </div>
+          <Post post={post} />
         )}
       </div>
     )
