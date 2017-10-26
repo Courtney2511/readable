8065e267c058308215e3c70fa1ac3ce7456ab45a
Courtney Noonan <courtneynoonan@me.com>
2017-10-26 14:04:33 -0400

    fixing typo in classname property

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends Component {
     return (
       <div className='footer'>
         <div className='container'>
-          <div class='content has-text-centered-text'>
+          <div className='content has-text-centered-text'>
             <p>
               <strong>readable</strong> by <a href="http://www.courtneynoonan.com">Courtney Noonan</a> is a
-                project for Udacity's React nanodegree.  
+                project for Udacity's React nanodegree.
             </p>
           </div>
         </div>
